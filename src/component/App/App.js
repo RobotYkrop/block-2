232762bb9777220604210c881b60f815eaf3c92f
RobotYkrop <dendozzor@gmail.com>
2022-07-05 14:17:38 +0500

    commit

---
+++ src/component/App/App.js
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <section className="movie">
+      <h1>Bibin</h1>
+    </section>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import App from './component/App/App';
+
+const app = document.getElementById('root');
+const root = createRoot(app);
+root.render(<App />);
